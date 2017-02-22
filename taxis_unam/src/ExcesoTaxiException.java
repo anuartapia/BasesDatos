d604419fd4731aa30fc5edeb9a068eeea4ffdcc1
Anuar <anuar@ciencias.unam.mx>
2017-02-21 23:42:35 -0600

    Agrega clases Exception para Dueno, Chofer y Taxi

---
+++ taxis_unam/src/ExcesoTaxiException.java
@@ -0,0 +1,14 @@
+/**
+ * ExcesoMedicoException.java
+ * Clase que implementa escepcion para cuando no se pueda guardar a otro medico
+ * @author lalo
+ * @version Noviembre 2016
+ */
+public class ExcesoTaxiException extends Exception{
+
+    public ExcesoTaxiException(String message){
+        super(message);
+    }
+
+}
+
